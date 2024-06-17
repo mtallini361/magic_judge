9d4bfa75836749fed7052e64a3026015280020d8
mtallini361 <mtallini361@gmail.com>
2024-06-17 16:35:53 -0400

    added functions to add nodes of different types

--- src/upload_graph_data.py
+++ src/upload_graph_data.py
@@ -27,3 +27,49 @@ class CardGraphDatabase:
                         flavor=card.flavor,
                         )
         return result.single()[0]
+    
+    @staticmethod
+    def _create_card_type_node(tx, card_type):
+        result = tx.run(
+            "CREATE (t:Type) ",
+            "SET t.name = $name ",
+            "RETURN t.name + 'added to node ' + id(t)",
+            name=card_type
+        )
+        return result.single()[0]
+    
+    @staticmethod
+    def _create_card_supertype_node(tx, card_supertype):
+        result = tx.run(
+            "CREATE (spr: SuperType) ",
+            "SET spr.name = $name ",
+            "RETURN spr.name + 'added to node ' + id(spr)",
+            name=card_supertype
+        )
+        return result.single()[0]
+    
+    @staticmethod
+    def _create_card_subtype_node(tx, card_subtype):
+        result = tx.run(
+            "CREATE (sub: SubType) ",
+            "SET sub.name = $name ",
+            "RETURN sub.name + 'added to node ' + id(sub)",
+            name=card_subtype
+        )
+        return result.single()[0]
+
+    @staticmethod
+    def _create_card_set_node(tx, card_set):
+        result = tx.run(
+            "CREATE (set: Set) ",
+            "SET set.name = $name ",
+            "SET set.block = $block ",
+            "SET set.release = $release ",
+            "SET set.type = $type ",
+            name=card_set.name,
+            block=card_set.block,
+            release=card_set.release_date,
+            type=card_set.type
+        )
+        return result.single()[0]
+
