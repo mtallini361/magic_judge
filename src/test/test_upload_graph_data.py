6246d231190284115ea95099909a68c8b1aa270b
mtallini361 <mtallini361@gmail.com>
2024-06-19 13:20:03 -0400

    added create and remove functions with tests

--- src/test/test_upload_graph_data.py
+++ src/test/test_upload_graph_data.py
@@ -1,10 +1,56 @@
 import neo4j
-from mtgsdk import Card
+from mtgsdk import Card, Type, Supertype, Subtype, Set
 from src.upload_graph_data import CardGraphDatabase
 
-
-def test_upload_card(mock_driver):
-    gdb = CardGraphDatabase("", "", "")
+def test_upload_remove_card():
+    gdb = CardGraphDatabase()
     card = Card.find("386616")
-    gdb.upload_card(card)
-    
+    uploaded = gdb.upload_card(card)
+    print(uploaded)
+
+    removed = gdb.remove_card(card)
+    print(removed)
+    gdb.close()
+
+def test_upload_remove_card_type():
+    gdb = CardGraphDatabase()
+    card_type = Type.all()[0]
+    uploaded = gdb.upload_card_type(card_type)
+    print(uploaded)
+
+    removed = gdb.remove_card_type(card_type)
+    print(removed)
+    gdb.close()
+
+def test_upload_remove_card_supertype():
+    gdb = CardGraphDatabase()
+    card_supertype = Supertype.all()[0]
+    uploaded = gdb.upload_card_supertype(card_supertype)
+    print(uploaded)
+
+    removed = gdb.remove_card_supertype(card_supertype)
+    print(removed)
+
+    gdb.close()
+
+def test_upload_remove_card_subtype():
+    gdb = CardGraphDatabase()
+    card_subtype = Subtype.all()[0]
+    uploaded = gdb.upload_card_subtype(card_subtype)
+    print(uploaded)
+
+    removed = gdb.remove_card_subtype(card_subtype)
+    print(removed)
+
+    gdb.close()
+
+def test_upload_remove_card_set():
+    gdb = CardGraphDatabase()
+    card_set = Set.find("ktk")
+    uploaded = gdb.upload_card_set(card_set)
+    print(uploaded)
+
+    removed = gdb.remove_card_set(card_set)
+    print(removed)
+
+    gdb.close()
--- src/upload_graph_data.py
+++ src/upload_graph_data.py
@@ -1,39 +1,130 @@
 import os
+import requests
+from requests.auth import HTTPBasicAuth
 from neo4j import GraphDatabase
 
 class CardGraphDatabase:
 
-    def __init__(self, uri, user, password):
-        self.driver = GraphDatabase.driver(uri, auth=(user, password))
+    def __init__(self, uri=None, user=None, password=None):
+        self.uri = uri
+        if self.uri is None:
+            self.uri = os.environ.get("NEO4J_URI")
+
+        self.user = user
+        if self.user is None:
+            self.user = os.environ.get("NEO4J_USERNAME")
+
+        self.password = password
+        if self.password is None:
+            self.password = os.environ.get("NEO4J_PASSWORD")
+
+        self.driver = GraphDatabase.driver(self.uri, auth=(self.user, self.password))
+
+    def request_token(self):
+        # Make the request to the Aura API Auth endpoint
+        response = requests.request(
+            "POST",
+            "https://api.neo4j.io/oauth/token",
+            headers={"Content-Type": "application/x-www-form-urlencoded"},
+            data={"grant_type": "client_credentials"},
+            auth=HTTPBasicAuth(self.user, self.password)
+        )
 
     def close(self):
         self.driver.close()
 
     def upload_card(self, card):
         with self.driver.session() as session:
-            session.execute_write(self._create_card_node, card)
+            result = session.execute_write(self._create_card_node, card)
+        return result
+
+    def remove_card(self, card):
+        with self.driver.session() as session:
+            result = session.execute_write(self._delete_card_node, card)
+        return result
+    
+    def upload_card_type(self, card_type):
+        with self.driver.session() as session:
+            result = session.execute_write(self._create_card_type_node, card_type)
+        return result
+    
+    def remove_card_type(self, card_type):
+        with self.driver.session() as session:
+            result = session.execute_write(self._delete_card_type_node, card_type)
+        return result
+    
+    def upload_card_supertype(self, supertype):
+        with self.driver.session() as session:
+            result = session.execute_write(self._create_card_supertype_node, supertype)
+        return result
+    
+    def remove_card_supertype(self, supertype):
+        with self.driver.session() as session:
+            result = session.execute_write(self._delete_card_supertype_node, supertype)
+        return result
+    
+    def upload_card_subtype(self, subtype):
+        with self.driver.session() as session:
+            result = session.execute_write(self._create_card_subtype_node, subtype)
+        return result
+
+    def remove_card_subtype(self, subtype):
+        with self.driver.session() as session:
+            result = session.execute_write(self._delete_card_subtype_node, subtype)
+        return result
+    
+    def upload_card_set(self, set):
+        with self.driver.session() as session:
+            result = session.execute_write(self._create_card_set_node, set)
+        return result
+    
+    def remove_card_set(self, set):
+        with self.driver.session() as session:
+            result = session.execute_write(self._delete_card_set_node, set)
+        return result
 
     @staticmethod
     def _create_card_node(tx, card):
-        result = tx.run("CREATE (a:Card) "
-                        "SET a.name = $name "
-                        "SET a.mana_cost = $mana_cost "
-                        "SET a.text = $text "
-                        "SET a.flavor = $flavor "
-                        "RETURN a.name + 'added to node ' + id(a)", 
-                        name=card.name,
-                        mana_cost=card.mana_cost,
-                        text=card.text,
-                        flavor=card.flavor,
-                        )
+        result = tx.run(
+            "CREATE (a:Card) "
+            "SET a.name = $name "
+            "SET a.mana_cost = $mana_cost "
+            "SET a.text = $text "
+            "SET a.flavor = $flavor "
+            "RETURN a.name + ' node created at id ' + id(a)", 
+            name=card.name,
+            mana_cost=card.mana_cost,
+            text=card.text,
+            flavor=card.flavor,
+        )
+        return result.single()[0]
+    
+    @staticmethod
+    def _delete_card_node(tx, card):
+        result = tx.run(
+            "MATCH (card: Card {name: $name}) "
+            "DELETE card "
+            "RETURN $name + ' node(s) deleted from id ' + id(card)",
+            name=card.name
+        )
         return result.single()[0]
     
     @staticmethod
     def _create_card_type_node(tx, card_type):
         result = tx.run(
-            "CREATE (t:Type) ",
-            "SET t.name = $name ",
-            "RETURN t.name + 'added to node ' + id(t)",
+            "CREATE (t:Type) "
+            "SET t.name = $name "
+            "RETURN t.name + ' node created at id ' + id(t)",
+            name=card_type
+        )
+        return result.single()[0]
+    
+    @staticmethod
+    def _delete_card_type_node(tx, card_type):
+        result = tx.run(
+            "MATCH (t:Type {name: $name}) "
+            "DELETE t "
+            "RETURN $name + ' node(s) deleted from id ' + id(t)",
             name=card_type
         )
         return result.single()[0]
@@ -41,9 +132,19 @@ class CardGraphDatabase:
     @staticmethod
     def _create_card_supertype_node(tx, card_supertype):
         result = tx.run(
-            "CREATE (spr: SuperType) ",
-            "SET spr.name = $name ",
-            "RETURN spr.name + 'added to node ' + id(spr)",
+            "CREATE (spr: SuperType) "
+            "SET spr.name = $name "
+            "RETURN spr.name + ' node created at id ' + id(spr)",
+            name=card_supertype
+        )
+        return result.single()[0]
+    
+    @staticmethod
+    def _delete_card_supertype_node(tx, card_supertype):
+        result = tx.run(
+            "MATCH (spr:SuperType {name: $name}) "
+            "DELETE spr "
+            "RETURN $name + ' node(s) deleted from id ' + id(spr)",
             name=card_supertype
         )
         return result.single()[0]
@@ -51,9 +152,19 @@ class CardGraphDatabase:
     @staticmethod
     def _create_card_subtype_node(tx, card_subtype):
         result = tx.run(
-            "CREATE (sub: SubType) ",
-            "SET sub.name = $name ",
-            "RETURN sub.name + 'added to node ' + id(sub)",
+            "CREATE (sub: SubType) "
+            "SET sub.name = $name "
+            "RETURN sub.name + ' node created at id ' + id(sub)",
+            name=card_subtype
+        )
+        return result.single()[0]
+    
+    @staticmethod
+    def _delete_card_subtype_node(tx, card_subtype):
+        result = tx.run(
+            "MATCH (sub:SubType {name: $name}) "
+            "DELETE sub "
+            "RETURN $name + ' node(s) deleted from id ' + id(sub)",
             name=card_subtype
         )
         return result.single()[0]
@@ -61,15 +172,26 @@ class CardGraphDatabase:
     @staticmethod
     def _create_card_set_node(tx, card_set):
         result = tx.run(
-            "CREATE (set: Set) ",
-            "SET set.name = $name ",
-            "SET set.block = $block ",
-            "SET set.release = $release ",
-            "SET set.type = $type ",
+            "CREATE (set: Set) "
+            "SET set.name = $name "
+            "SET set.block = $block "
+            "SET set.release = $release "
+            "SET set.type = $type "
+            "RETURN set.name + ' node created at id ' + id(set)",
             name=card_set.name,
             block=card_set.block,
             release=card_set.release_date,
             type=card_set.type
+        )
+        return result.single()[0]
+    
+    @staticmethod
+    def _delete_card_set_node(tx, card_set):
+        result = tx.run(
+            "MATCH (set: Set {name: $name}) "
+            "DELETE set "
+            "RETURN $name + ' node(s) deleted from id ' + id(set)",
+            name=card_set.name
         )
         return result.single()[0]
     
